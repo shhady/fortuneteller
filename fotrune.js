24db8d1aa3baf5190040bc7d5d325afc00bb26af
shhady <shhadyse@gmail.com>
2022-04-05 23:43:49 +0300

    fortuneteller

---
+++ fotrune.js
@@ -0,0 +1,5 @@
+function tellFortune(jobTitle,Location,partnerName,numberOfChildren){
+    console.log(`You will be a ${jobTitle} in ${Location} and married to
+    ${partnerName} with ${numberOfChildren} children.`);
+}
+tellFortune('Programmer', 'TelAviv', 'Sharon', 2);
